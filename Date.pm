cdde70cbcd24ccb5073e2a0628df01cb4a2d2e7d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-14 21:32:50 +0100

    Prepare for 0.03 version

--- Date.pm
+++ Date.pm
@@ -11,7 +11,7 @@ use Readonly;
 
 Readonly::Array our @EXPORT_OK => qw(check_date check_date_dmy check_date_order);
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub check_date {
 	my ($self, $key) = @_;
@@ -277,6 +277,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
